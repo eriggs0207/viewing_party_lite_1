10e42918031045769dd838105df2e72c7f9bd0df
Erik Riggs <riggs.erik@gmail.com>
2022-10-24 16:23:53 -0600

    user story 2 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "#{@user.name} profile successfully created"
       redirect_to user_path(@user)
+    elsif params[:password] != params[:password_confirmation]
+      flash[:error] = "passwords do not match"
+      redirect_to '/register/new'
     else
       redirect_to '/register/new'
     end
@@ -24,6 +28,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :email }
+    it { should have_secure_password }
   end
   describe 'relationships' do
     it { should have_many :user_parties }
